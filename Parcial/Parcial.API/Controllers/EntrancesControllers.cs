6aa26a5092406722290eb49334f2b830a7568d23
DiegoArias1234 <diegoarias245794@correo.itm.edu.co>
2023-04-29 01:02:41 -0500

    Begin Entrances

---
+++ Parcial/Parcial.API/Controllers/EntrancesControllers.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Parcial.API.Data;
+using Parcial.Shared.Entities;
+
+namespace Parcial.API.Controllers
+{
+    [ApiController]
+    [Route("/api/entrances")]
+    public class EntrancesControllers : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public EntrancesControllers(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok( await _context.Entrances.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Entrance entrance)
+        {
+            _context.Add(entrance);
+            await _context.SaveChangesAsync();
+            return Ok(entrance);
+        }
+    }
+}
