4c48f2570de9029ea3dec05953458501b944ca7f
DiegoArias1234 <diegoarias245794@correo.itm.edu.co>
2023-04-29 08:45:54 -0500

    Complete entrance

--- Parcial/Parcial.API/Controllers/EntrancesControllers.cs
+++ Parcial/Parcial.API/Controllers/EntrancesControllers.cs
@@ -7,11 +7,11 @@ namespace Parcial.API.Controllers
 {
     [ApiController]
     [Route("/api/entrances")]
-    public class EntrancesControllers : ControllerBase
+    public class EntrancesController : ControllerBase
     {
         private readonly DataContext _context;
 
-        public EntrancesControllers(DataContext context)
+        public EntrancesController(DataContext context)
         {
             _context = context;
         }
@@ -22,6 +22,18 @@ namespace Parcial.API.Controllers
             return Ok( await _context.Entrances.ToListAsync());
         }
 
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var entrance = await _context.Entrances.FirstOrDefaultAsync(X => X.Id == id);
+            if (entrance == null)
+            {
+                return NotFound();
+            }
+            return Ok(entrance);
+        }
+
+
         [HttpPost]
         public async Task<ActionResult> PostAsync(Entrance entrance)
         {
@@ -29,5 +41,28 @@ namespace Parcial.API.Controllers
             await _context.SaveChangesAsync();
             return Ok(entrance);
         }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Entrance entrance)
+        {
+            _context.Update(entrance);
+            await _context.SaveChangesAsync();
+            return Ok(entrance);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var entrance = await _context.Entrances.FirstOrDefaultAsync(X => X.Id == id);
+            if (entrance == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(entrance);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
+
+
